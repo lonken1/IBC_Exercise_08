149566f00dca069c639bf659cd02a53850a6ce68
lonken <lonken@nd.edu>
2019-11-21 20:43:12 -0500

    Finished Problem 2

--- ANSWERS.R
+++ ANSWERS.R
@@ -19,8 +19,10 @@ msuscore <- 0
 # THE for loop. It adds to cummulative score depending on the team.
 # It then creates a vector for both UW and MSU containing the time of the score and the current score of that team.
 # Finally, rbind combines the vector just created with the dataframe that has the cummulative score over time.
+# For uw, uwscore=current score, uwtimescore=current time and current total, gamescoreuw=dataframe of every time and score.
 
 for (i in 1:nrow(data)){
+  
   if (data[i,'team']=='UW'){
     uwscore <- uwscore+data[i,'score']
     uwtimescore <- c(data[i,'time'],uwscore)
@@ -28,6 +30,7 @@ for (i in 1:nrow(data)){
     gamescoreuw <- rbind(gamescoreuw,uwtimescore)
     gamescoremsu <- rbind(gamescoremsu,msutimescore)
   }
+  
   else {
     msuscore <- msuscore+data[i,'score']
     uwtimescore <- c(data[i,'time'],uwscore)
@@ -38,5 +41,41 @@ for (i in 1:nrow(data)){
   
 }
 
+# This plots the msu score line first and then the uw scoreline
 plot(x=gamescoremsu$time,y=gamescoremsu$score,type='l',xlab = "Time",ylab = "Score")
 lines(x=gamescoreuw$time,y=gamescoreuw$score)
+
+# PROBLEM NUMBER 2
+
+# Preset amount and guess to zero
+amount <- 0
+guess <- 0
+
+# Generate a random number
+number <- sample(1:100, 1)
+
+# Print I am thinking of a number
+print("I'm thinking of a number 1-100...")
+
+# The while loop continues as long as the amount does not reach 10 and exits if guessed correctly
+while(amount!= 10){
+  guess <- readline(prompt = "Guess:")
+  amount=amount+1
+  if (guess<number){
+    print("Higher")
+  }
+  else if (guess>number){
+    print("Lower")
+  }
+  if (guess==number){
+    print("Correct!")
+    return()
+  }
+  if (amount==10){
+    print("Sorry! Too many guesses")
+  }
+}
+
+
+
+
